6a1c049840b07e0bb4ec02e39795ffb812d38162
Chuck Meyer <chuck.meyer@algolia.com>
2022-04-11 18:41:16 -0400

    Fix typo

--- src/config/settings.js
+++ src/config/settings.js
@@ -30,7 +30,7 @@ import { Hit } from './Hit';
 |
 */
 
-export const inputContainer = 'header-bard__module.header-bar__search';
+export const inputContainer = 'header-bar__module.header-bar__search';
 export const inputContent = 'Search for products';
 export const keyboardShortcuts = ['/'];
 export const appId = 'H2M6B61JEG';
