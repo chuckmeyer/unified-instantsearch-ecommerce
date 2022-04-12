5156865386a7b832d5031dd39790808a62314e6f
Chuck Meyer <chuck.meyer@algolia.com>
2022-04-12 10:28:59 -0400

    Fixed container name

--- src/config/settings.js
+++ src/config/settings.js
@@ -30,7 +30,7 @@ import { Hit } from './Hit';
 |
 */
 
-export const inputContainer = '.site-header__icon-wrapper';
+export const inputContainer = '.site-header__icons-wrapper';
 export const inputContent = 'Search for products';
 export const keyboardShortcuts = ['/'];
 export const appId = 'H2M6B61JEG';
